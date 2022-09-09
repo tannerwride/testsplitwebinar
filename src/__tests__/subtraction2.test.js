05392376c01060c4dde2014520ddb13e591c1eb8
Catherine Pope <catherine.pope@circleci.com>
2022-09-09 15:13:27 +0100

    Added test

---
+++ src/__tests__/subtraction2.test.js
@@ -0,0 +1,15 @@
+import { subtract } from '../math';
+
+describe('Subtraction testing', () => {
+      test('14 - 1 = 13', () => {
+        expect(subtract(14, 1)).toBe(13);
+      });
+
+      test('26 - 1 = 15', () => {
+        expect(subtract(26, 1)).toBe(15);
+      });
+
+      test('110 - 1 = 109', () => {
+        expect(subtract(110, 1)).toBe(109);
+      });
+})
