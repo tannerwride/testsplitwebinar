6ee37f391509bd6e125d6a4c54e395e122d978ca
Catherine Pope <catherine.pope@circleci.com>
2022-09-09 15:09:39 +0100

    Added test

--- src/__tests__/addition2.test.js
+++ src/__tests__/addition2.test.js
@@ -1,23 +1,23 @@
 import { add } from '../math';
 
 describe('Addition testing', () => {
-    test('2 + 2 = 4', () => {
-        expect(add(2, 2)).toBe(4);
+    test('5 + 2 = 7', () => {
+        expect(add(5, 2)).toBe(7);
       });
 
-      test('2 + 3 = 5', () => {
-        expect(add(2, 3)).toBe(5);
+      test('12 + 3 = 15', () => {
+        expect(add(12, 3)).toBe(15);
       });   
 
-      test('2 + 5 = 7', () => {
-        expect(add(2, 5)).toBe(7);
+      test('22 + 15 = 37', () => {
+        expect(add(22, 15)).toBe(37);
       });    
 
-      test('2 + 7 = 9', () => {
-        expect(add(2, 7)).toBe(9);
+      test('32 + 17 = 49', () => {
+        expect(add(32, 17)).toBe(49);
       });    
 
-      test('2 + 20 = 22', () => {
-        expect(add(2, 20)).toBe(22);
+      test('42 + 20 = 62', () => {
+        expect(add(42, 20)).toBe(62);
       });
 })
